b4d730fae44048ad02a51ac158a949a32870b092
Randall <randalltr@proton.me>
2022-07-04 09:15:57 +0700

    mapped my_range function to str type conversion operator

--- 13_ranges_of_strings.py
+++ 13_ranges_of_strings.py
@@ -11,4 +11,4 @@ my_range = range(1, 21)
 # Expected output:
 # ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 
-print(map(num_to_str, *my_range))
+print(list(map(str, my_range)))
