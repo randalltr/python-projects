2ea17c9b387fda55dd89acf0d647ea5a4f61fe00
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-13 10:37:20 +0700

    first attempt at adding function

--- 20_apply_fx_to_dict.py
+++ 20_apply_fx_to_dict.py
@@ -4,3 +4,13 @@ d = {"a": 1, "b": 2, "c": 3}
 
 # Expected output:
 #  6
+
+
+def add_function(x):
+    sum = 0
+    for v in x.values():
+        sum += v
+    return sum
+
+
+print(add_function(d))
