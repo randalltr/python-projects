007fb72725d6b863d9a58656da48f3844f555f76
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-14 08:52:55 +0700

    solved dictionary filtering with new dict and for loop on items

--- 21_dictionary_filtering.py
+++ 21_dictionary_filtering.py
@@ -1,9 +1,14 @@
-# Question: Filter the dictionary by removing all items with a value of 
+# Question: Filter the dictionary by removing all items with a value of
 # greater than 1.
 
 d = {"a": 1, "b": 2, "c": 3}
 
-# Expected output: 
-# {'a': 1}  
+# Expected output:
+# {'a': 1}
 
-print(x in d.values() if x <= 1)
+e = {}
+for k, v in d.items():
+    if v <= 1:
+        e[k] = v
+
+print(e)
