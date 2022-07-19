2a6524281df3f2efe81a0a3030082102d4b8e654
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-19 12:22:26 +0700

    added question 22 and printed full dictionary

---
+++ 22_formatted_print.py
@@ -0,0 +1,7 @@
+# Create a dictionary of keys a, b, c, where each key has a value
+# from list 1 to 10, 11 to 20, and 21 to 30 respectively and print out
+
+d = {"a": list(range(1, 11)), "b": list(
+    range(11, 21)), "c": list(range(21, 31))}
+
+print(d)
