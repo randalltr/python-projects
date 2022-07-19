8942b1e6320c3051d288995d28e638292ea38356
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-19 12:23:17 +0700

    format printed dictionary

--- 22_formatted_print.py
+++ 22_formatted_print.py
@@ -4,4 +4,9 @@
 d = {"a": list(range(1, 11)), "b": list(
     range(11, 21)), "c": list(range(21, 31))}
 
-print(d)
+
+print(
+    f"Key 'a' is {d['a']}\n"
+    f"Key 'b' is {d['b']}\n"
+    f"Key 'c' is {d['c']}\n"
+)
