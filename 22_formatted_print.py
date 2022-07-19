bb05ab5c254e398ed1e6038d9091af7f657e86b9
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-19 12:24:37 +0700

    added pprint module and used alternative format printing

--- 22_formatted_print.py
+++ 22_formatted_print.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 # Create a dictionary of keys a, b, c, where each key has a value
 # from list 1 to 10, 11 to 20, and 21 to 30 respectively and print out
 
@@ -5,8 +7,10 @@ d = {"a": list(range(1, 11)), "b": list(
     range(11, 21)), "c": list(range(21, 31))}
 
 
-print(
-    f"Key 'a' is {d['a']}\n"
-    f"Key 'b' is {d['b']}\n"
-    f"Key 'c' is {d['c']}\n"
-)
+# print(
+#     f"Key 'a' is {d['a']}\n"
+#     f"Key 'b' is {d['b']}\n"
+#     f"Key 'c' is {d['c']}\n"
+# )
+
+pprint(d)
