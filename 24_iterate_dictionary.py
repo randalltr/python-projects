8dde550b460845fbbf4b5dae63c52df55a03dc49
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-20 09:22:50 +0700

    q24 solved with for loop over keys in dict

--- 24_iterate_dictionary.py
+++ 24_iterate_dictionary.py
@@ -10,4 +10,5 @@ d = dict(a=list(range(1, 11)), b=list(range(11, 21)), c=list(range(21, 31)))
 # c has value [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 # a has value [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-pprint(d)
+for i in d.keys():
+    print(f"{i} has value {d[i]}")
