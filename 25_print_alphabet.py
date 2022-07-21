89f65e55bf5d9af96b555cacd6a6fc8ad21f2ee8
Randall <108246257+randalltr@users.noreply.github.com>
2022-07-21 10:46:27 +0700

    q25 alternative solution using string import

--- 25_print_alphabet.py
+++ 25_print_alphabet.py
@@ -1,6 +1,11 @@
 # Make a script that prints out letters of alphabet from a to z
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-for i in alphabet:
-    print(i)
+# for i in alphabet:
+#     print(i)
+
+import string
+
+for letter in string.ascii_lowercase:
+    print(letter)
